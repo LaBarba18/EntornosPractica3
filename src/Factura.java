f2f38a2ec6f24aa942dc881c0956289f3b84c966
LaBarba18 <52631457+LaBarba18@users.noreply.github.com>
2020-02-09 20:03:08 +0100

    sexto commit

--- src/Factura.java
+++ src/Factura.java
@@ -2,7 +2,7 @@ public class Factura {
     public int num;
     public float base;
     public float tipoIva;
-    public Estados estado;                      //IMPLEMENTAR enum Estados
+    public Estados estado;
 
     public static void main(String[] args){
 
